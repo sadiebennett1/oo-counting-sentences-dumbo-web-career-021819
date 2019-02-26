dd78b1bac6f8e9f3a2ca680a0484b6b6849f6c44
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-26 17:18:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,21 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with(".")
+    if self.end_with?(".")
       return true 
     end
     false 
   end
 
   def question?
-   if self.end_with("?")
+   if self.end_with?("?")
       return true 
     end
     false 
   end
 
   def exclamation?
-   if self.end_with("!")
+   if self.end_with?("!")
       return true 
     end
     false 
