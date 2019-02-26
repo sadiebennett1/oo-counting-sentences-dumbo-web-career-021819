ab929c70d26306156beb5cb28640e34f0c0c07d9
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-26 17:17:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with(".")
+      return true 
+    end
+    false 
   end
 
   def question?
-
+   if self.end_with("?")
+      return true 
+    end
+    false 
   end
 
   def exclamation?
-
+   if self.end_with("!")
+      return true 
+    end
+    false 
   end
 
   def count_sentences
-
+    arr = self.split("!", ".", "?")
+    new_arr
+    arr.each do |sent|
+      if sent != ""
+        new_arr << sent
+      end
+    end
+    return new_arr.length
+        
+    
   end
 end
